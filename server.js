a034c6a8f0af4d3c9c6f9a5362879360f93aca7e
Adam Haag <adam.m.h@gmail.com>
2019-04-06 20:59:51 -0400

    testing

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
+const mongojs = require('mongojs');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/api/connection', (req, res) => {
-    res.status(200).json({connection: "success"});
+//Define CRUD routes
+app.get('/api/todoList', (req, res) => {
+  db.todoItems.find({}, (err, result) => {
+    if(err) res.status(500).json(err);
+    else res.status(200).json(result);
+  });
 });
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/dist/todo"));
-  app.get('*', (req, res) => {    //send the client application for all non-api calls
+  app.get('*', (req, res) => {    
+    //send the client application for all non-api calls
     res.sendFile(path.join(__dirname, "./client/dist/todo/index.html"));
   });
 }
-
-mongoose.Promise = Promise;
-const mongoEnv = process.env.MONGODB_URI || 'mongodb://localhost/todo';
-mongoose.connect(mongoEnv, {useNewUrlParser: true});
-const db = mongoose.connection;
-
+console.log({"mongo_uri": process.env.MONGODB_URI});
+const db = mongojs(process.env.MONGODB_URI || 'mongodb://localhost/todo', ['todoItems']);  
 db.on("error", (error) => console.log("Mongoose Error: ", error));
 
-db.once("open", () => {
-  console.log("Mongoose connection successful.");
-  app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
